b26e4ede7a7c6ff2a391dd82df002d40a5930ee1
Sayo Ogunlegan <ogunlegano@gmail.com>
2019-11-05 14:47:50 -0800

    updates

---
+++ cmd/todo/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/b1ackmartian/todo"
+)
+
+// TodoFIleName Environment variable to read filename from
+const TodoFileName = "TODO_FILENAME"
+
+var todoFileName = ".todo.json"
+
+func main() {
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	if os.Getenv(TodoFileName) != "" {
+		todoFileName = os.Getenv(TodoFileName)
+	}
+
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	switch {
+	case *list:
+		fmt.Print(l)
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != "":
+		l.Add(*task)
+
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/b1ackmartian/todo
+
+go 1.12
